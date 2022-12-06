c488cc4beef1d87b94f2223c373caa4e7501767f
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-12-06 11:42:39 +0000

    wip: add toast to edit button

--- packages/jaen/src/internal/components/atoms/EditActionButton/useEdit.ts
+++ packages/jaen/src/internal/components/atoms/EditActionButton/useEdit.ts
@@ -1,10 +1,29 @@
+import {useModals} from '../../../context/Modals/ModalContext.js'
 import {useStatus} from '../../../hooks/useStatus.js'
 
 export function useEdit() {
   const status = useStatus()
 
+  const {toast} = useModals()
+
+  const toggleEditing = () => {
+    if (status.isEditing) {
+      toast({
+        title: 'Editing mode disabled',
+        description: 'You can now view the content of this page'
+      })
+    } else {
+      toast({
+        title: 'Editing mode enabled',
+        description: 'You can now edit the content of this page'
+      })
+    }
+
+    status.toggleIsEditing()
+  }
+
   return {
     isEditing: status.isEditing,
-    toggleEditing: status.toggleIsEditing
+    toggleEditing
   }
 }
