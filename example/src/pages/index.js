741e3ce28c8debaf3ee32aa7cfde57d49f4a7cbf
Nico Schett <52858351+schettn@users.noreply.github.com>
2023-02-04 22:48:02 +0000

    wip(example): add section field to index page

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,8 +1,92 @@
 import * as React from "react"
-import { Button } from "@chakra-ui/react"
-import { connectPage, Field, useWidget } from "@snek-at/jaen"
+import { Button, Box, Image, Badge, Wrap, WrapItem } from "@chakra-ui/react"
+import { StarIcon } from "@chakra-ui/icons"
+import { connectPage, Field, useWidget, connectBlock } from "@snek-at/jaen"
 
 import { graphql } from "gatsby"
+
+const Card = connectBlock(
+  () => {
+    const property = {
+      imageUrl: "https://bit.ly/2Z4KKcF",
+      imageAlt: "Rear view of modern home with pool",
+      beds: 3,
+      baths: 2,
+      title: "Modern home in city center in the heart of historic Los Angeles",
+      formattedPrice: "$1,900.00",
+      reviewCount: 34,
+      rating: 4,
+    }
+
+    return (
+      <Box w="sm" borderWidth="1px" borderRadius="lg">
+        {/* <Image src={property.imageUrl} alt={property.imageAlt} /> */}
+
+        <Box h="md">
+          <Field.Image
+            name="image"
+            label="Image"
+            // defaultValue={property.imageUrl}
+          />
+        </Box>
+
+        <Box p="6">
+          <Box display="flex" alignItems="baseline">
+            <Badge borderRadius="full" px="2" colorScheme="teal">
+              New
+            </Badge>
+            <Box
+              color="gray.500"
+              fontWeight="semibold"
+              letterSpacing="wide"
+              fontSize="xs"
+              textTransform="uppercase"
+              ml="2"
+            >
+              {property.beds} beds &bull; {property.baths} baths
+            </Box>
+          </Box>
+
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            // lineHeight="tight"
+            noOfLines={1}
+          >
+            <Field.Text
+              name="title"
+              label="Title"
+              defaultValue={property.title}
+            />
+          </Box>
+
+          <Box>
+            {property.formattedPrice}
+            <Box as="span" color="gray.600" fontSize="sm">
+              / wk
+            </Box>
+          </Box>
+
+          <Box display="flex" mt="2" alignItems="center">
+            {Array(5)
+              .fill("")
+              .map((_, i) => (
+                <StarIcon
+                  key={i}
+                  color={i < property.rating ? "teal.500" : "gray.300"}
+                />
+              ))}
+            <Box as="span" ml="2" color="gray.600" fontSize="sm">
+              {property.reviewCount} reviews
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    )
+  },
+  { name: "card", label: "House Card" }
+)
 
 const IndexPage = connectPage(
   () => {
@@ -22,12 +106,21 @@ const IndexPage = connectPage(
     }, [])
 
     return (
-      <div style={{ color: `purple`, fontSize: `72px`, height: "5000px" }}>
-        <h1>Hello Gatsby!</h1>
-        <p>Now go build something great.</p>
-        <Button colorScheme="teal">Button</Button>
-        <Field.Text name="test" defaultValue={"teest"} label="Test" />
-      </div>
+      <>
+        <Field.Section
+          name="section"
+          label="Section"
+          blocks={[Card]}
+          as={Wrap}
+          sectionAs={WrapItem}
+        />
+        <div style={{ color: `purple`, fontSize: `72px`, height: "5000px" }}>
+          <h1>Hello Gatsby!</h1>
+          <p>Now go build something great.</p>
+          <Button colorScheme="teal">Button</Button>
+          <Field.Text name="test" defaultValue={"teest"} label="Test" />
+        </div>
+      </>
     )
   },
   {
