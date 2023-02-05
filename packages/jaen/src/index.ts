dad07242df25c2812b574635c6575964bdc1ff2e
Nico Schett <52858351+schettn@users.noreply.github.com>
2023-02-05 19:35:22 +0000

    wip: update jaen entry

--- packages/jaen/src/index.ts
+++ packages/jaen/src/index.ts
@@ -8,6 +8,7 @@ export {
   connectView
 } from './connectors/index.js'
 export * from './fields/index.js'
+export {useStatus} from './internal/hooks/useStatus.js'
 export {useWidget} from './internal/hooks/useWidget.js'
 export * as internal from './internal/index.js'
 export type {IJaenPage, SiteMetadata} from './types.js'
--- packages/jaen/src/unstable-node.ts
+++
@@ -1,3 +0,0 @@
-export {generatePageOriginPath} from './internal/helper/path.js'
-export * as JaenSource from './utils/JaenSource.js'
-export {convertToSlug} from './utils/path.js'
