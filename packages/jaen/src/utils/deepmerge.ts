f5c75af47ac6a8b76069b21c7324d52d45b7d504
Nico Schett <nicoschett@icloud.com>
2023-06-28 00:06:06 +0200

    WIP: fix deepmergeArrayIdMerge null issue
    Fix Cannot read properties of null (reading 'id')

--- packages/jaen/src/utils/deepmerge.ts
+++ packages/jaen/src/utils/deepmerge.ts
@@ -5,22 +5,34 @@ export const deepmergeArrayIdMerge = (
   source: any[],
   options: any
 ) => {
+  // Check if target or source is null or undefined
+  if (target == null) {
+    return source ? source.slice() : []
+  }
+
+  if (source == null) {
+    return target.slice()
+  }
+
   const groups = ['id', 'fieldName']
 
   for (const group of groups) {
-    if (target.every(v => v[group]) && source.every(v => v[group])) {
+    if (
+      target.every(v => v?.[group]) && // Check if target elements and group property exist
+      source.every(v => v?.[group]) // Check if source elements and group property exist
+    ) {
       const mergeArrays = (arr1: any[] = [], arr2: any[] = [], key = 'id') => {
         const elements: any[] = []
 
         const arr2Copy = arr2.slice()
 
         for (const element of arr1) {
-          const el = arr2Copy.find(v => v[key] === element[key])
+          const el = arr2Copy.find(v => v && v[key] === element[key]) // Check if element and key property exist
 
           if (el) {
             elements.push(
               deepmerge(element, el, {
-                ...options,
+                ...(options || {}),
                 arrayMerge: deepmergeArrayIdMerge
               })
             )
@@ -49,7 +61,7 @@ export const deepmergeArrayIdMerge = (
       destination[index] = options.cloneUnlessOtherwiseSpecified(item, options)
     } else if (options.isMergeableObject(item)) {
       destination[index] = deepmerge(target[index], item, {
-        ...options,
+        ...(options || {}),
         arrayMerge: deepmergeArrayIdMerge
       })
     } else if (!target.includes(item)) {
