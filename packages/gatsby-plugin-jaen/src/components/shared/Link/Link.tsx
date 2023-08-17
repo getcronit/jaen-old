86935561b7ae4a800c8cf7c40fb4592bc4b0993e
Nico Schett <nicoschett@icloud.com>
2023-08-17 09:34:50 +0200

    save

--- packages/gatsby-plugin-jaen/src/components/shared/Link/Link.tsx
+++ packages/gatsby-plugin-jaen/src/components/shared/Link/Link.tsx
@@ -1,6 +1,6 @@
 import {As, Link as ChakraLink} from '@chakra-ui/react'
 import {Link as GatsbyLink} from 'gatsby'
-import React from 'react'
+import React, {forwardRef} from 'react'
 
 import {isInternalLink} from './utils/is-internal-path'
 
@@ -11,18 +11,25 @@ export interface LinkProps<T extends As> extends React.ComponentProps<any> {
   children: React.ReactNode
 }
 
-export function Link<T extends As>({as, to, ...props}: LinkProps<T>) {
+const Link = <T extends As>(
+  {as, to, ...props}: LinkProps<T>,
+  ref: React.Ref<HTMLAnchorElement | HTMLButtonElement>
+) => {
   const Wrapper = as || ChakraLink
 
   if (to && !props.isDisabled) {
     const isInternal = isInternalLink(to)
 
     if (isInternal) {
-      return <Wrapper as={GatsbyLink} to={to} {...props} />
+      return <Wrapper as={GatsbyLink} to={to} {...props} ref={ref} />
     }
 
-    return <Wrapper as="a" href={to} {...props} />
+    return <Wrapper as="a" href={to} {...props} ref={ref} />
   }
 
-  return <Wrapper {...props} />
+  return <Wrapper {...props} ref={ref} />
 }
+
+const ForwardedLink = forwardRef(Link)
+
+export {ForwardedLink as Link}
