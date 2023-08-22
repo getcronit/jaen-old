577ff720dd5958ef4b074d4f9a56389a057c3fad
Nico Schett <nicoschett@icloud.com>
2023-08-23 00:10:36 +0200

    save

---
+++ packages/gatsby-jaen-lens/src/pages/lens/service.tsx
@@ -0,0 +1,54 @@
+import {Box, Text} from '@chakra-ui/react'
+import {PageConfig, PageProps} from '@snek-at/jaen'
+import {useEffect, useRef, useState} from 'react'
+
+const Page: React.FC<PageProps> = ({data, location}) => {
+  const [fqdn, setFqdn] = useState<string | null>(null)
+
+  useEffect(() => {
+    // get faq from location hash
+    const fqdn = location.hash.slice(1)
+
+    // set fqdn to state
+    setFqdn(fqdn)
+
+    window.scrollTo(0, 0)
+  }, [
+    location.hash // only update when location.hash changes
+  ])
+
+  return (
+    <Box h="calc(100dvh - 4rem)" w="full">
+      {fqdn && (
+        <iframe
+          sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-top-navigation-by-user-activation allow-top-navigation"
+          src={`https://${fqdn}`}
+          style={{width: '100%', height: 'calc(100dvh - 4rem)', border: 'none'}}
+        />
+      )}
+    </Box>
+  )
+}
+
+export const pageConfig: PageConfig = {
+  label: 'Lens Service',
+  layout: {
+    name: 'jaen',
+    type: 'full'
+  },
+  breadcrumbs: [
+    {
+      label: 'Lens',
+      path: '/lens/'
+    },
+    {
+      label: 'Service',
+      path: '/lens/service/'
+    }
+  ],
+  withoutJaenFrameStickyHeader: true
+}
+
+export default Page
+
+export {Head} from '@snek-at/jaen'
